5fc9f6d4e02bd838bdccd3bd1e7da9625d626beb
jmirkovi <jmirkovi@MIRKOVICJOSA321>
2019-03-14 16:25:01 +0100

    Add example project for NET45 and confluent stable sdk

---
+++ src/EventBus.Lab.KafkaExample45-old/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Lab.KafkaExample45_old
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string brokerList = "[NameSpace]:9093";
+            string connectionString = "[ConnectionString]";
+            string topic = "carinfo";
+            string caCertLocation = "cacert.pem";
+            string consumerGroup = "$Default";
+
+            Console.WriteLine("Initializing Producer");
+            Worker.Producer(brokerList, connectionString, topic, caCertLocation).Wait();
+            Console.WriteLine();
+            Console.WriteLine("Initializing Consumer");
+            Worker.Consumer(brokerList, connectionString, consumerGroup, topic, caCertLocation);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ src/EventBus.Lab.KafkaExample45-old/Worker.cs
@@ -0,0 +1,82 @@
+﻿using Confluent.Kafka;
+using Confluent.Kafka.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Lab.KafkaExample45_old
+{
+    public static class Worker
+    {
+        public static async Task Producer(string brokerList, string connStr, string topic, string cacertlocation)
+        {
+            try
+            {
+                var config = new Dictionary<string, object> {
+                    { "bootstrap.servers", brokerList },
+                    { "security.protocol", "SASL_SSL" },
+                    { "sasl.mechanism", "PLAIN" },
+                    { "sasl.username", "$ConnectionString" },
+                    { "sasl.password", connStr },
+                    { "ssl.ca.location", cacertlocation },
+                    //{ "debug", "security,broker,protocol" }       //Uncomment for librdkafka debugging information
+                };
+
+                using (var producer = new Producer<long, string>(config, new LongSerializer(), new StringSerializer(Encoding.UTF8)))
+                {
+                    Console.WriteLine("Sending 2 messages to topic: " + topic + ", broker(s): " + brokerList);
+                    for (int x = 0; x < 2; x++)
+                    {
+                        var msg = string.Format("Sample message #{0} sent at {1}", x, DateTime.Now.ToString("yyyy-MM-dd_HH:mm:ss.ffff"));
+                        var deliveryReport = await producer.ProduceAsync(topic, DateTime.UtcNow.Ticks, msg);
+                        Console.WriteLine(string.Format("Message {0} sent (value: '{1}')", x, msg));
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(string.Format("Exception Occurred - {0}", e.Message));
+            }
+        }
+
+        public static void Consumer(string brokerList, string connStr, string consumergroup, string topic, string cacertlocation)
+        {
+            var config = new Dictionary<string, object> {
+                    { "bootstrap.servers", brokerList },
+                    { "security.protocol","SASL_SSL" },
+                    { "sasl.mechanism","PLAIN" },
+                    { "sasl.username", "$ConnectionString" },
+                    { "sasl.password", connStr },
+                    { "ssl.ca.location", cacertlocation },
+                    { "group.id", consumergroup },
+                    { "request.timeout.ms", 60000 },
+                    { "broker.version.fallback", "1.0.0" },         //Event Hubs for Kafka Ecosystems supports Kafka v1.0+, 
+                                                                    //a fallback to an older API will fail
+                    //{ "debug", "security,broker,protocol" }       //Uncomment for librdkafka debugging information
+                };
+
+            using (var consumer = new Consumer<long, string>(config, new LongDeserializer(), new StringDeserializer(Encoding.UTF8)))
+            {
+                consumer.OnMessage += (_, msg)
+                  => Console.WriteLine($"Received: '{msg.Value}'");
+
+                consumer.OnError += (_, error)
+                  => Console.WriteLine($"Error: {error}");
+
+                consumer.OnConsumeError += (_, msg)
+                  => Console.WriteLine($"Consume error ({msg.TopicPartitionOffset}): {msg.Error}");
+
+                Console.WriteLine("Consuming messages from topic: " + topic + ", broker(s): " + brokerList);
+                consumer.Subscribe(topic);
+
+                while (true)
+                {
+                    consumer.Poll(TimeSpan.FromMilliseconds(1000));
+                }
+            }
+
+        }
+    }
+}
